5cc06e6098e653b4de78e86744b4ec7f64436108
Sergey Khval <gluks2@yandex.ru>
2013-07-04 12:17:13 +0300

    Added web.js and index.html

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+var hello = fs.readFileSync("index.html").toString("utf-8");
+
+app.get('/', function(request, response) {
+  response.send(hello);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
